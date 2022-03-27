6a8119108d7ac5ef8f3c686f0e768602a86e6fd3
Amanda Luu <mandydl33@gmail.com>
2022-03-27 14:12:56 -0700

    fixed gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
 
-
-gulp.task('sass', function(){
+gulp.task('sass', function() {
 	return gulp.src('src/scss/app.scss')
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 		.pipe(gulp.dest('app/css'));
 });
 
-gulp.task('default');
+
+gulp.task('default', ['sass']);
