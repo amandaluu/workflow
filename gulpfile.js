bbc9eb8e1524c2d8175c8050866cbe6f4947b519
Amanda Luu <mandydl33@gmail.com>
2022-03-27 20:17:43 -0700

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,32 @@ const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
+var SOURCEPATH = {
+	sassSource : 'src/scss/*.scss'
+}
+
+var APPPATH = {
+	root : 'app/',
+	css : 'app/css',
+	js : 'app/js'
+}
 
 gulp.task('sass', function() {
-	return gulp.src('src/scss/app.scss')
+	return gulp.src(SOURCEPATH.sassSource)
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest('app/css'));
+		.pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('serve', ['sass'], function() {
+	browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+		server : {
+			baseDir : APPPATH.root
+		}
+	})
+})
 
-gulp.task('default', ['sass']);
+gulp.task('watch', ['serve', 'sass'], function() {
+	gulp.watch([SOURCEPATH.sassSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);
